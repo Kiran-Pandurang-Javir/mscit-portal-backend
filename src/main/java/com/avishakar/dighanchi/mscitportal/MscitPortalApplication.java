a87dd4d2e2189c88d5c6c67470013d3c4a4b6dcc
kiran javir <kiranjavir001@gmail.com>
2025-09-11 19:13:22 +0530

    Initial commit - backend

---
+++ src/main/java/com/avishakar/dighanchi/mscitportal/MscitPortalApplication.java
@@ -0,0 +1,11 @@
+package com.avishakar.dighanchi.mscitportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscitPortalApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MscitPortalApplication.class, args);
+	}
+}
